d86f88423c1257bec43d8f5e35df3b62cd2a1a30
klanti117 <klantyislam@yahoo.com>
2023-07-22 21:05:39 -0400

    initial setup

--- day0/src/App.js
+++ day0/src/App.js
@@ -1,7 +1,8 @@
 import logo from './logo2.png';
 import './App.css';
-
+import Card from './components/card'
 function App() {
+  let items = [1,2,3,4.5,6]
   return (
     <div className="App">
       <header className="App-header">
@@ -11,7 +12,10 @@ function App() {
       <br/>
       <br/>
       <br/>
-      <h1>Day0: Boiler plate code </h1>
+      <h1>Day3: Random Color </h1>
+      {
+        items.map(()=><Card/>)
+      }
     </div>
   );
 }
