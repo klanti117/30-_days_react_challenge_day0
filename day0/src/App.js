a6c303784e43ecdbd1f222065fad586283d15254
klanti117 <klantyislam@yahoo.com>
2023-07-27 08:56:56 -0400

    initial code

--- day0/src/App.js
+++ day0/src/App.js
@@ -1,17 +1,41 @@
-import logo from './logo2.png';
 import './App.css';
+const numbers = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31]
 
+// const cardStyles = {
+//   backgroundColor: '#93c572',
+//   padding: '1rem',
+//   border: '2px solid white',
+//   color:'black'
+// }
+
+const containerStyles = {
+  padding: '1rem',
+  display:'grid',
+  gridTemplateColumns: 'repeat(auto-fit, minmax(200px, 1fr))',
+}
+
+const Number = ({number}) =>{
+  return (
+    <div style={{backgroundColor: number%2===0? 'purple': 'green', 
+      padding: '1rem',
+      border: '2px solid white',
+      color:'black' 
+    }}>{number}</div>
+  )
+}
+
+const NumberList = ({numbers}) => {
+  const numberList = numbers.map((number)=><Number number={number}/>)
+  return (
+    <div style={containerStyles}>{numberList}</div>
+  )
+}
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-      </header>
-      {/* TODO: these br are here so that the logo doesnot rotate over text */}
-      <br/>
-      <br/>
-      <br/>
-      <h1>Day0: Boilerplate code </h1>
+    <div className='App'>
+      <h1>30 Days of React Challenge</h1>
+      <p>Number Generator</p>
+      <NumberList  numbers={numbers}/>
     </div>
   );
 }
